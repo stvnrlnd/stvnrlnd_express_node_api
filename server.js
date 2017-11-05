a6f335c8aeaafcbb1747d1a85d4c6fe8f2c19c15
Steven Roland <steven@mojave.io>
2017-11-05 16:30:42 -0500

    Added port var, config for api and server vars, and server

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 // ------ Configure the application
+var port = process.env.PORT || 8080;
 
+var server = express();
+var api = express.Router();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 // ------ Build the routes
 
 
 // ------ Serve the public
+server.listen(port, function() {
+    console.log("Running on port", port);
+});
