609731263f6d4e6d790d34d1e8ccfab8f8a1dff3
Steven Roland <steven@mojave.io>
2017-11-05 11:45:18 -0500

    Used npm to install express, body-parser, and mongoose. Required them in server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 'use strict';
 
 // ------ Require the packages
-
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 // ------ Configure the application
 
