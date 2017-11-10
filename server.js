e89d0ff0101877f369315a9faff6e99d12bbd332
Steven Roland <steven@mojave.io>
2017-11-10 11:54:33 -0500

    Added /users route with get and post method

--- server.js
+++ server.js
@@ -18,11 +18,33 @@ server.use(bodyParser.json());
 
 // ------ Build the routes
 api.route('/')
-.get(function(req, res) {
-    res.json({
-        "message": "Hello, beautiful!"
+    .get(function(req, res) {
+        res.json({
+            "message": "Hello, beautiful!"
+        });
     });
-});
+
+api.route('/users')
+    .post(function(req, res) { // Create a new user
+        var user = new User();
+        user.name = req.body.name;
+        user.save(function(err) {
+            if (err){
+                res.send(err);
+            } else {
+                res.json({ message: 'User created!' });
+            }
+        });
+    })
+    .get(function(req, res) { // Return all users
+        User.find(function(err, users) {
+            if (err){
+                res.send(err);
+            } else {
+                res.json(users);
+            }
+        });
+    });
 
 server.use('/api', api);
 
